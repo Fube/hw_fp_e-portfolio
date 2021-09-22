1b5ece65cbe8e8dcc6e2acfd33703fe01cecb4db
Fube <fubeitch@gmail.com>
2021-09-21 20:08:09 -0400

    Added basics

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Tutorial/>
+  <div class="text-center bg-primary h-screen font-body pt-6">
+    <header class="">
+      <h1 class="inline text-6xl text-quinary">Nariman</h1>
+      &nbsp;
+      <h1 class="inline text-6xl text-senary">Abrari</h1>
+      <h2 class="mt-4 text-4xl font-sub text-ternary">PetClinic</h2>
+    </header>
+    <main></main>
+  </div>
 </template>
 
 <script lang="ts">
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: '#212429',
+      secondary: '#133727',
+      ternary: '#56dca1',
+      quaternary: '#3da175',
+      quinary: '#4FB0AC',
+      senary: '#B04F53',
+    },
+    fontFamily: {
+      body: ['JetBrains Mono', 'monospace'],
+      sub: ['Montserrat', 'sans-serif'],
+      content: ['Nunito', 'sans-serif'],
+    },
+  },
+}
